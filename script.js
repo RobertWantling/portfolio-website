0c4509b868fc3b556752175622a14e28eadca077
RobertWantling <robert6995@hotmail.co.uk>
2023-05-18 18:49:30 +0100

    added more photos for site and refactored menu fade animation

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 const btnsCloseModal = document.querySelector(".btn--close-modal");
+const nav = document.querySelector(".nav");
 
 // Open modal
 const openModal = function (e) {
@@ -124,93 +125,48 @@ const slider = function () {
 };
 slider(); // create big function better practise
 
-// dont want 1st slide 0%, 2nd will start at 100%, 200%, 300%
-// want first slide to be -100%, then 0%, 100%, 200% - how? Take current index and subtract the current slide
-// curSlide = 1: first iteration will be 0 - so 0 - 100  * 100 = -100
-// then next iteration 1 - 1 = 0%
-// need to tell JS to stop - need to define no. of slides and then make it stop once reached last one
-//////////////////////////////////////////////////////
-// const slider = function () {
-// const slides = document.querySelectorAll(".slide");
-// const btnLeft = document.querySelector(".slider__btn--left");
-// const btnRight = document.querySelector(".slider__btn--right");
-// const dotContainer = document.querySelector(".dots");
-//
-// let curSlide = 0;
-// const maxSlide = slides.length;
-//
-// Functions;
-// const createDots = function () {
-// slides.forEach(function (_, i) {
-// dotContainer.insertAdjacentHTML(
-// "beforeend",
-// `<button class="dots__dot" data-slide="${i}"></button>`
-// );
+// Menu fade animation
+const handleHover = function (e) {
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target; // creating a variable which contains el working with
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link"); // need to select other el can go to parent and select
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+  }
+};
+// Passing an 'argument' into handler
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+
+nav.addEventListener("mouseout", handleHover.bind(1));
+
+// Sticky navigation - use scroll event
+// const obsCallback = function (entries, observer) {
+// entries.forEach((entry) => {
+// console.log(entries);
 // });
 // };
 //
-// const activateDot = function (slide) {
-// document
-// .querySelectorAll(".dots__dot")
-// .forEach((dot) => dot.classList.remove("dots__dot--active"));
-//
-// document
-// .querySelector(`.dots__dot[data-slide="${slide}"]`)
-// .classList.add("dots__dot--active");
+// const obsOptions = {
+// root: null,
+// threshold: [0, 0.2],
 // };
+// const observer = new IntersectionObserver(obsCallback, obsOptions);
+// observer.observe(about);
 //
-// const goToSlide = function (slide) {
-// slides.forEach(
-// (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
-// );
-// };
-//
-// Next slide
-// const nextSlide = function () {
-// if (curSlide === maxSlide - 1) {
-// curSlide = 0;
-// } else {
-// curSlide++;
-// }
-//
-// goToSlide(curSlide);
-// activateDot(curSlide);
-// };
-//
-// const prevSlide = function () {
-// if (curSlide === 0) {
-// curSlide = maxSlide - 1;
-// } else {
-// curSlide--;
-// }
-// goToSlide(curSlide);
-// activateDot(curSlide);
-// };
-//
-// const init = function () {
-// goToSlide(0);
-// createDots();
-//
-// activateDot(0);
-// };
-// init();
-//
-//Event handlers
-// btnRight.addEventListener("click", nextSlide);
-// btnLeft.addEventListener("click", prevSlide);
-//
-// document.addEventListener("keydown", function (e) {
-// if (e.key === "ArrowLeft") prevSlide();
-// e.key === "ArrowRight" && nextSlide();
-// });
-//
-// dotContainer.addEventListener("click", function (e) {
-// if (e.target.classList.contains("dots__dot")) {
-// const { slide } = e.target.dataset;
-// goToSlide(slide);
-// activateDot(slide);
-// }
-// });
-// };
-// slider();
-//
+const header = document.querySelector(".intro");
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries; // same as entires[0]
+
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(intro);
