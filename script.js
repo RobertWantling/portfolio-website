7048196b1cc173f16da29ec681e1ac68585543bb
RobertWantling <robert6995@hotmail.co.uk>
2023-06-07 15:01:24 +0100

    reveal elements on scroll

--- script.js
+++ script.js
@@ -153,14 +153,15 @@ nav.addEventListener("mouseout", handleHover.bind(1));
 // };
 // const observer = new IntersectionObserver(obsCallback, obsOptions);
 // observer.observe(about);
-//
+
+// Sticky Nav
 const header = document.querySelector(".intro");
 const navHeight = nav.getBoundingClientRect().height;
 
 const stickyNav = function (entries) {
   const [entry] = entries; // same as entires[0]
 
-  if (!entry.isIntersecting) nav.classList.add("sticky");
+  if (!entry.isIntersecting) nav.classList.add("sticky", "txt-change");
   else nav.classList.remove("sticky");
 };
 
@@ -172,9 +173,32 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 headerObserver.observe(intro);
 
 // Reveal sections
-// const allSections = document.querySelectorAll(".section");
-//
-// const revealSection = function (entries, observer) {};
-//
-// const sectionObserver = new IntersectionObserver(revealSection, {});
-//
+const allSections = document.querySelectorAll(".section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
+
+// selected section nav link
+
+function activateNav(el) {
+  const selected = document.getElementsByTagName("a");
+  for (i = 0; i < selected.length; i++) {
+    selected[i].classList.remove("active");
+  }
+  el.classList.add("active");
+}
